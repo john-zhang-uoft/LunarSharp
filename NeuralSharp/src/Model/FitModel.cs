34891eccac21305aebe7c3ea658022f0476bd92b
johnz <johnzhanguoft@gmail.com>
2021-07-25 23:14:41 -0400

    Added validation set into fit method, restructured code

--- NeuralSharp/src/Model/FitModel.cs
+++ NeuralSharp/src/Model/FitModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using static NeuralSharp.Sampling;
 
 namespace NeuralSharp
 {
@@ -31,28 +34,49 @@ namespace NeuralSharp
             {
                 throw new InvalidDataException("X and Y are not the same shape.");
             }
-            
+
             Console.WriteLine($"Starting training process with verbosity: {verbose}.");
 
             // For each epoch
             for (int e = 0; e < epochs; e++)
             {
                 string message = $"Epoch: {e + 1}\t";
-             
+
+                if (shuffle)
+                {
+                    x.Shuffle();
+                    y.Shuffle();
+                }
+
+                List<Matrix> xTrain = new List<Matrix>();
+                List<Matrix> yTrain = new List<Matrix>();
+                List<Matrix> xVal = new List<Matrix>();
+                List<Matrix> yVal = new List<Matrix>();
+
+                if (validationFrac > 0)
+                {
+                    (xTrain, yTrain, xVal, yVal) = TrainValSplitList(x, y, validationFrac);
+                }
+                else
+                {
+                    xTrain = x.ToList();
+                    yTrain = y.ToList();
+                }
+
                 // For each batch
-                for (int i = 0; i < x.Length / batchSize; i++)
+                for (int i = 0; i < xTrain.Count / batchSize; i++)
                 {
                     // Reset gradients in each layer
                     foreach (Layer l in _layers)
                     {
                         l.ResetGradients();
                     }
-                    
+
                     // For each datapoint inside that batch
                     for (int j = 0; j < batchSize; j++)
                     {
-                        ForwardPass(x[i * batchSize + j]);
-                        BackwardPass(x[i * batchSize + j], y[i * batchSize + j], alpha, gamma);
+                        ForwardPass(xTrain[i * batchSize + j]);
+                        BackwardPass(xTrain[i * batchSize + j], yTrain[i * batchSize + j], alpha, gamma);
                     }
 
                     // Update gradient based on the mean gradient
@@ -61,24 +85,24 @@ namespace NeuralSharp
                         l.UpdateParameters(batchSize, alpha, gamma);
                     }
                 }
-                
+
                 // Reset gradients in each layer
                 foreach (Layer l in _layers)
                 {
                     l.ResetGradients();
                 }
-                
+
                 // For each remaining datapoint
-                for (int i = (x.Length / batchSize) * batchSize; i < x.Length; i++)
+                for (int i = (xTrain.Count / batchSize) * batchSize; i < xTrain.Count; i++)
                 {
-                    ForwardPass(x[i]);
-                    BackwardPass(x[i], y[i], alpha, gamma);
+                    ForwardPass(xTrain[i]);
+                    BackwardPass(xTrain[i], yTrain[i], alpha, gamma);
                 }
-                
+
                 // Update gradient based on the mean gradient
                 foreach (Layer l in _layers)
                 {
-                    l.UpdateParameters(batchSize, alpha, gamma);
+                    l.UpdateParameters(xTrain.Count - (xTrain.Count / batchSize * batchSize), alpha, gamma);
                 }
 
                 // Validate data
@@ -89,10 +113,112 @@ namespace NeuralSharp
 
                     trainLoss += _lossFunction(_layers[^1].Neurons, y[i]);
                 }
+
                 trainLoss /= x.Length;
-                
+
                 message += $"Train loss = {trainLoss}";
                 
+                // Validation data
+                if (validationFrac != 0)
+                {
+                    float valLoss = 0;
+
+                    for (int i = 0; i < xVal.Count; i++)
+                    {
+                        Predict(xVal[i]);
+                        valLoss += _lossFunction(_layers[^1].Neurons, yVal[i]);
+                    }
+
+                    valLoss /= xVal.Count;
+
+                    message += $"Validation loss = {valLoss}";
+                }
+                
+                Console.WriteLine(message);
+            }
+        }
+
+        public void Fit2(Matrix[] x, Matrix[] y, int batchSize = 1, int epochs = 1, int verbose = 1,
+            float alpha = 0.001f, float gamma = 0.001f, Callback[] callbacks = null, float validationFrac = 0,
+            bool shuffle = false, float[] classWeights = null, float[] datasetWeights = null)
+        {
+            // Check whether x and y are the same length
+            if (x.Length != y.Length)
+            {
+                throw new InvalidDataException("X and Y are not the same shape.");
+            }
+
+            if (validationFrac < 0)
+            {
+                throw new InvalidModelArgumentException("Validation fraction cannot be less than zero");
+            }
+
+            Console.WriteLine($"Starting training process with verbosity: {verbose}.");
+
+            // For each epoch
+            for (int e = 0; e < epochs; e++)
+            {
+                string message = $"Epoch: {e + 1}\t";
+
+                if (shuffle)
+                {
+                    x.Shuffle();
+                    y.Shuffle();
+                }
+
+                Matrix[] xTrain = new Matrix[] { };
+                Matrix[] yTrain = new Matrix[] { };
+                Matrix[] xVal = new Matrix[] { };
+                Matrix[] yVal = new Matrix[] { };
+
+                if (validationFrac > 0)
+                {
+                    (xTrain, yTrain, xVal, yVal) = TrainValSplit(x, y, validationFrac);
+                }
+                else
+                {
+                    xTrain = x;
+                    yTrain = y;
+                }
+
+                // For each batch
+                for (int i = 0; i < xTrain.Length / batchSize * batchSize; i += batchSize)
+                {
+                    TrainBatch(xTrain[i..(i + batchSize)],
+                        yTrain[i..(i + batchSize)], alpha, gamma);
+                }
+
+                TrainBatch(xTrain[(xTrain.Length / batchSize * batchSize).. xTrain.Length],
+                    yTrain[(xTrain.Length / batchSize * batchSize).. xTrain.Length], alpha, gamma);
+
+                // Validate train data
+                float trainLoss = 0;
+                for (int i = 0; i < xTrain.Length; i++)
+                {
+                    Predict(xTrain[i]);
+                    trainLoss += _lossFunction(_layers[^1].Neurons, yTrain[i]);
+                }
+
+                trainLoss /= xTrain.Length;
+
+                message += $"Train loss = {trainLoss}";
+
+                // Validation data
+                if (validationFrac != 0)
+                {
+                    float valLoss = 0;
+
+                    for (int i = 0; i < xVal.Length; i++)
+                    {
+                        Predict(xVal[i]);
+                        valLoss += _lossFunction(_layers[^1].Neurons, yVal[i]);
+                    }
+
+                    valLoss /= xVal.Length;
+
+                    message += $"Validation loss = {valLoss}";
+                }
+
                 Console.WriteLine(message);
             }
         }
---
+++ NeuralSharp/src/Sampling.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace NeuralSharp
+{
+    public static class Sampling
+    {
+        private static readonly Random Rand = new Random();
+        
+        /// <summary>
+        /// Randomly orders the elements in a list and does not return a new list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1) {  
+                n--;
+                int k = Rand.Next(n + 1);  
+                T value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }  
+        }
+
+        /// <summary>
+        /// Returns list of n randomly chosen elements in a random order.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="n"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> Sample<T>(this IEnumerable<T> list, int n)
+        {
+            Random rand = new Random();
+            
+            return list.OrderBy(x => rand.Next()).Take(n);
+        }
+
+        /// <summary>
+        /// Returns a training list and a validation list of elements with sizes based on validationFrac.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="validationFrac"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static (List<T> train, List<T> val) TrainValSplit<T>(T[] list, float validationFrac)
+        {
+            int numVal = (int) Math.Round(list.Length * validationFrac); // number of items to select
+            List<T> train = new List<T>();
+            List<T> val = new List<T>();
+            
+            // Store current number of needed items and number of available ones left to select from 
+            double needed = numVal;
+            double available = list.Length;
+
+            // Iterate through list
+            // The probability that an element is selected is needed / available,
+            // Guaranteeing that the required number of elements are selected in one pass through
+            for (int i = 0; i < list.Length; i++)
+            {   // If the element is randomly selected
+                if (Rand.NextDouble() < needed / available)
+                {
+                    val.Add(list[i]);
+                    needed--;
+                }
+                else
+                {
+                    train.Add(list[i]);
+                }
+                available--;
+            }
+            
+            return (train, val);
+        }
+
+        /// <summary>
+        /// Returns a training list and a validation list of elements with sizes based on validationFrac.
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="validationFrac"></param>
+        /// <param name="x"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static (List<T> xTrain, List<T> yTrain, List<T> xVal, List<T> yVal) TrainValSplitList<T>(T[] x, T[] y, float validationFrac)
+        {
+            if (x.Length != y.Length)
+            {
+                throw new InvalidDataException("x and y are not the same length.");
+            }
+            
+            List<T> xTrain = new List<T>();
+            List<T> yTrain = new List<T>();
+            
+            List<T> xVal = new List<T>();
+            List<T> yVal = new List<T>();
+            
+            // Store current number of needed items and number of available ones left to select from 
+            
+            double needed = (int) Math.Round(x.Length * validationFrac); // number of items to select;
+            double available = x.Length;
+
+            // Iterate through list
+            // The probability that an element is selected is needed / available,
+            // Guaranteeing that the required number of elements are selected in one pass through
+            for (int i = 0; i < x.Length; i++)
+            {   // If the element is randomly selected
+                if (Rand.NextDouble() < needed / available)
+                {
+                    xVal.Add(x[i]);
+                    yVal.Add(y[i]);
+                    needed--;
+                }
+                else
+                {
+                    xTrain.Add(x[i]);
+                    yTrain.Add(y[i]);
+                }
+                available--;
+            }
+            
+            return (xTrain, yTrain, xVal, yVal);
+        }
+        
+        /// <summary>
+        /// Returns a training list and a validation list of elements with sizes based on validationFrac.
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="validationFrac"></param>
+        /// <param name="x"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static (T[] xTrain, T[] yTrain, T[] xVal, T[] yVal) TrainValSplit<T>(T[] x, T[] y, float validationFrac)
+        {
+            if (x.Length != y.Length)
+            {
+                throw new InvalidDataException("x and y are not the same length.");
+            }
+
+            int numVal = (int) Math.Round(x.Length * validationFrac);
+                
+            // Store current number of needed items and number of available ones left to select from 
+            
+            double neededVal = numVal; // number of items to select for val;
+            double neededTrain = x.Length - numVal;
+            double available = x.Length;
+
+            T[] xTrain = new T[(int) (x.Length - neededVal)];
+            T[] yTrain = new T[(int) (x.Length - neededVal)];
+            
+            T[] xVal = new T[(int) neededVal];
+            T[] yVal = new T[(int) neededVal];
+            
+            // Iterate through list
+            // The probability that an element is selected is needed / available,
+            // Guaranteeing that the required number of elements are selected in one pass through
+            for (int i = 0; i < x.Length; i++)
+            {   // If the element is randomly selected
+                if (Rand.NextDouble() < neededVal / available)
+                {
+                    xVal[numVal - (int) neededVal] = x[i];
+                    yVal[numVal - (int) neededVal] = y[i];
+                    neededVal--;
+                }
+                else
+                {
+                    xTrain[x.Length - (int) neededTrain] = x[i];
+                    yTrain[x.Length - (int) neededTrain] = y[i];
+                    neededTrain--;
+                }
+                available--;
+            }
+            
+            return (xTrain, yTrain, xVal, yVal);
+        }
+    }
+}
